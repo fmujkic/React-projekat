e9ce07c1b8caf5503d28320de985cf7608c871a6
Fejsal Mujkic <fmujkic2@etf.unsa.bam>
2021-07-18 13:22:22 +0200

    kreiranje socketa

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-function Chat() {
+let socket;
+const ENDPOINT = 'localhost:5000';
+
+
+const Chat  = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+
+    setName(name);
+    setRoom(room);
+
+
+socket.emit("join",{name,room})
+
+
+  },[ENDPOINT,location.search]);
+
   return <h1>Chat</h1>;
 }
 
