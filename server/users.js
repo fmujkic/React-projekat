4c343311e4a66a81f8ce9fe506924cead1da6206
Fejsal Mujkic <fmujkic2@etf.unsa.bam>
2021-07-18 19:57:39 +0200

    prepravljene funkcije

--- server/users.js
+++ server/users.js
@@ -18,19 +18,19 @@ const addUser = ({ id, name, room }) => {
 
   users.push(user);
 
-  return{ user };
+  return { user };
 };
 
 const removeUser = (id) => {
-    const index = users.findIndex((user) => user.id=== id);
+  const index = users.findIndex((user) => user.id === id);
 
-    if(index !== -1){
-        return users.splice(index,1)[0];
-    }
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
 };
 
-const getUser = (id) => users.find((user)=> user.id===id)
+const getUser = (id) => users.find((user) => user.id === id);
 
-const getUsersInRoom = (room) => users.filter((user)=> user.room === room);
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
-module.exports = {addUser,removeUser,getUser,getUsersInRoom};
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
