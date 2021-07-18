9f0efdac31fb3dc39a3a1ead6c8f57c4f712c78a
Fejsal Mujkic <fmujkic2@etf.unsa.bam>
2021-07-18 15:54:14 +0200

    podesavanje socketa za poruke

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -8,6 +8,8 @@ const ENDPOINT = "localhost:5000";
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -19,14 +21,41 @@ const Chat = ({ location }) => {
 
     socket.emit("join", { name, room }, () => {});
 
-    return ()=>{
-      socket.emit('disconnect');
+    return () => {
+      socket.emit("izlaz");
 
       socket.off();
-    }
-  }, [ENDPOINT, location.search]);
+    };
+  }, [location.search]);
 
-  return <h1>Chat</h1>;
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+console.log(message,messages);
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <input
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyPress={(event) =>
+            event.key === "Enter" ? sendMessage(event) : null
+          }
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
+
 const PORT = process.env.PORT || 5000;
 
 const router = require("./router");
-const { Socket } = require("dgram");
 
 const app = express();
 
@@ -15,13 +16,46 @@ const io = socketio(server);
 io.on("connection", (socket) => {
   console.log("Uspostavljena konekcija");
 
-  socket.on("join", ({name,room},callback) => {
-    console.log(name,room);
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+
+    socket.join(user.room);
+
+
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, dobrodošao u sobu ${user.room}.`
+    });
+
+
+    socket.broadcast.to(user.room).emit("message", {
+      user: "admin",
+      text: `${user.name} se pridruzio `
+    });
+
 
     callback();
   });
 
-  socket.on("disconnect", () => {
+
+
+  socket.on("sendMessage", (message,callback) => {
+
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", {
+      user: user.name,
+      text: message,
+    });
+    callback();
+  });
+
+
+
+  socket.on("izlaz", () => {
     console.log("Korisnik je napustio");
   });
 });
